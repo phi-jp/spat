6ec128241e6f4c6c1154caea48d9d23c3b8c067e
phi <phi1618jp@gmail.com>
2017-02-10 15:42:12 +0900

    build

--- spat.js
+++ spat.js
@@ -124,7 +124,7 @@ riot.tag2('spat-nav', '<div ref="pages" class="spat-pages"></div> <div if="{_loc
 
       var page = this.refs.pages.querySelector('[data-is=' + tagName + ']');
 
-      if(!page) {
+      if(!page || page.dataset.is === tagName) {
         page = document.createElement('div');
         page.classList.add('spat-page');
         this.refs.pages.appendChild(page);
