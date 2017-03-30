e753501413cf09c086279963bb7ea73083495558
phi <phi1618jp@gmail.com>
2017-03-30 23:25:23 +0900

    use pug

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ var header = require('gulp-header');
 var sort = require('gulp-sort');
 
 var pkg = require('./package.json');
-var target = ['./src/*.jade', './src/**/*.jade'];
+var target = ['./src/*.pug', './src/**/*.pug'];
 var output = './';
 
 var banner = `
@@ -35,7 +35,7 @@ var spat = {
 gulp.task('riot', function() {
   gulp
     .src(target)
-    .pipe(riot({template:'jade'}))
+    .pipe(riot({template:'pug'}))
     .pipe(sort())
     .pipe(concat('spat.js'))
     .pipe(header(banner, {
